cbb438f96f5e85d9f434344e96e445c43e964d47
Ahmed Zougari <github@ahmedz.dev>
2024-07-16 16:49:48 +0100

    add base to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zougari47.github.io',
+  base: '/portfolio-template',
   integrations: [tailwind()],
 })
